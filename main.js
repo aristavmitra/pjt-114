f79bf539189765def48f4561d53cf7657c817089
aristavmitra <117516264+aristavmitra@users.noreply.github.com>
2024-02-02 17:10:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+function setup(){
+  canvas = createCanvas(400,400);
+  video = createCapture(VIDEO);
+  video.hide()
+  canvas.center()
+}
+function draw(){
+  image(video,0,0,400,400);
+}
